07cb8aa5165a2b1da23d23c14d30601601822392
VitalyTen <vtenigin@student.42.us.org>
2017-03-05 15:06:35 -0800

    moduser.php

--- admin.php
+++ admin.php
@@ -7,20 +7,39 @@ include("header.php");
 <main class="content">
 <h1>ADMIN</h1>
 <?php
+
 if (file_exists("./private/user"))
 {
 	$tab = unserialize(file_get_contents("./private/user"));
-	echo "<form action='admin.php' method='POST'>";
+
+
+
 	foreach ($tab as $user)
 	{
-		echo "<p><input type='number' name='id' value=".$user["id"].">";
-		echo "<input type='text' name='fname' value=".$user["fname"].">";
-		echo "<input type='text' name='lname' value=".$user["lname"].">";
-		echo "<input type='checkbox' name='admin'";
+		echo "<form action='moduser.php' method='POST'>";
+		echo "<p><input type='number' name=".$user["id"]." value=".$user["id"]." readonly>";
+		echo "<input type='email' name='email".$user["id"]."' value=".$user["email"].">";
+		echo "<input type='text' name='fname".$user["id"]."' value=".$user["fname"].">";
+		echo "<input type='text' name='lname".$user["id"]."' value=".$user["lname"].">";
+		echo "<input type='checkbox' name='admin".$user["id"]."' value='admin'";
 		if ($user["admin"])
 			echo " checked";
-		echo "><input type='submit' name='submit' value='apply'>";
+		echo "><input type='submit' name='submit".$user["id"]."' value='apply'></form>";
 	}
+	// foreach ($tab as $user)
+	// {
+	// 	if ($_POST["submit".$user["id"]] === "apply")
+	// 	{
+	// 		$user["email"] = $_POST["email".$user["id"]];
+	// 		$user["fname"] = $_POST["fname".$user["id"]];
+	// 		$user["lname"] = $_POST["lname".$user["id"]];
+	// 		// if (isset($_POST["admin".$user["id"]])
+	// 		// 	$user["admin"] = 1;
+	// 		// else
+	// 		// 	$user["admin"] = 0;
+	// 	}
+	// }
+	// file_put_contents("./private/user", serialize($tab));
 }
 ?>
 </main>
---
+++ moduser.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+if (file_exists("./private/user"))
+{
+	$user = unserialize(file_get_contents("./private/user"));
+	$i = -1;
+	while (++$i < count($user))
+	{
+		if ($_POST["submit".$user[$i]["id"]] == "apply")
+		{
+			$user[$i]["email"] = $_POST["email".$user[$i]["id"]];
+			$user[$i]["fname"] = $_POST["fname".$user[$i]["id"]];
+			$user[$i]["lname"] = $_POST["lname".$user[$i]["id"]];
+			if ($_POST["admin".$user[$i]["id"]] == "admin")
+				$user[$i]["admin"] = 1;
+			else
+				$user[$i]["admin"] = 0;
+		}
+	}
+	file_put_contents("./private/user", serialize($user));
+
+
+	// foreach ($tab as $user)
+	// {
+	// 	echo "<form action='admin.php' method='POST'>";
+	// 	echo "<p><input type='number' name=".$user["id"]." value=".$user["id"]." readonly>";
+	// 	echo "<input type='email' name='email".$user["id"]."' value=".$user["email"].">";
+	// 	echo "<input type='text' name='fname".$user["id"]."' value=".$user["fname"].">";
+	// 	echo "<input type='text' name='lname".$user["id"]."' value=".$user["lname"].">";
+	// 	echo "<input type='checkbox' name='admin".$user["id"]."'";
+	// 	if ($user["admin"])
+	// 		echo " checked";
+	// 	echo "><input type='submit' name='submit".$user["id"]."' value='apply'></form>";
+	// }
+}
+header("Location: admin.php");
+?>
