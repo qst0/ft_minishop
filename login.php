798ab9f65849b2d57982535ef6fc1b984a26e741
VitalyTen <vtenigin@student.42.us.org>
2017-03-04 18:34:44 -0800

    error.php

--- login.php
+++ login.php
@@ -1,14 +1,44 @@
 <?php
 session_start();
+function auth($email, $passwd)
+{
+	if (file_exists("./private/user"))
+	{
+		$passwd = hash("whirlpool", $passwd);
+		$tab = unserialize(file_get_contents("./private/user"));
+		foreach ($tab as $user)
+			if ($email === $user["email"] && $passwd === $user["passwd"])
+				return (TRUE);
+	}
+	return (FALSE);
+}
+
+if (isset($_GET["submit"]))
+{
+	if ($_GET["email"] != "" && $_GET["passwd"] != "")
+	{
+		$_SESSION["loggued_on_user"] = "";
+		if (auth($_GET["email"], $_GET["passwd"]))
+		{
+			$_SESSION["loggued_on_user"] = $_GET["email"];
+			header("Location: index.php");
+			exit();
+		}
+	}
+	header("Location: error.php");
+	exit();
+}
+
+
 include("header.php");
 ?>
 <div class="login">
 	<div class="login-triangle"></div>
 	<h2 class="login-header">Log in</h2>
-	<form class="login-container">
-		<p><input type="email" placeholder="Email"></p>
-		<p><input type="password" placeholder="Password"></p>
-		<p><input type="submit" value="LOG IN"></p>
+	<form class="login-container" action="login.php" method="GET">
+		<p><input type="email" placeholder="Email" name="email" value=""></p>
+		<p><input type="password" placeholder="Password" name="passwd" value=""></p>
+		<p><input type="submit" name="submit" value="log in"></p>
 	</form>
 </div>
 <?php include("footer.php"); ?>
--- register.php
+++ register.php
@@ -1,16 +1,46 @@
 <?php
 session_start();
+if (isset($_POST["submit"]))
+{
+	if ($_POST["fname"] != "" && $_POST["lname"] != "" && $_POST["email"] != "" && $_POST["passwd"] != "")
+	{
+		$user["fname"] = $_POST["fname"];
+		$user["lname"] = $_POST["lname"];
+		$user["email"] = $_POST["email"];
+		$user["passwd"] = hash("whirlpool", $_POST["passwd"]);
+		if (!file_exists("./private"))
+			mkdir("./private");
+		if (file_exists("./private/user"))
+		{
+			$tab = unserialize(file_get_contents("./private/user"));
+			foreach ($tab as $val)
+			{
+				if ($user["email"] === $val["email"])
+				{
+					header("Location: error.php");
+					exit();
+				}
+			}
+		}
+		$tab[] = $user;
+		file_put_contents("./private/user", serialize($tab));
+		header("Location: index.php");
+		exit();
+	}
+	header("Location: error.php");
+	exit();
+}
 include("header.php");
 ?>
 <div class="login">
 	<div class="login-triangle"></div>
 	<h2 class="login-header">CREATE ACCOUNT</h2>
-	<form class="login-container">
-		<p><input type="text" placeholder="First Name"></p>
-		<p><input type="text" placeholder="Last Name"></p>
-		<p><input type="email" placeholder="Email"></p>
-		<p><input type="password" placeholder="Password"></p>
-		<p><input type="submit" value="CREATE AN ACCOUNT"></p>
+	<form class="login-container" action="register.php" method="POST">
+		<p><input type="text" name="fname" placeholder="First Name" value=""></p>
+		<p><input type="text" name="lname" placeholder="Last Name" value=""></p>
+		<p><input type="email" name="email" placeholder="Email" value=""></p>
+		<p><input type="password" name="passwd" placeholder="Password" value=""></p>
+		<p><input type="submit" name="submit" value="Register"></p>
 	</form>
 </div>
 <?php include("footer.php"); ?>
